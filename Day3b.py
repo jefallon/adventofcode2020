22db6448c3896491615ad6d3cc76d12490e75a34
Joshua Fallon <fallonje@adelphia.net>
2020-12-03 17:54:52 -0500

    Fix Day3b.py

--- Day3b.py
+++ Day3b.py
@@ -5,20 +5,21 @@ def countTrees(infile):
 	trees = [0,0,0,0,0]
 	slopes = [1,3,5,7]
 	skip = 0
+	horiz = [0,0,0,0,0]
 	with open(infile, 'r') as f:
-		horiz = [0,0,0,0,0]
 		for row in f:
 			treeline = row.strip()
 			for i,spot in enumerate(horiz[:4]):
 				if treeline[spot] == '#':
 					trees[i] += 1
-			horiz[i] += 3
-			horiz[i] %= 31
-			if not skip:
-				if treeline[horiz[-1]] == '#':
-					trees[-1] += 1
-				horiz[-1] += 1
-				horiz[-1] %= 31
+				horiz[i] += slopes[i]
+				horiz[i] %= 31
+			if skip == 0:
+				if treeline[horiz[4]] == '#':
+					trees[4] += 1
+			else:
+				horiz[4] += 1
+				horiz[4] %= 31
 			skip ^= 1
 
 	return reduce(mul, trees)
