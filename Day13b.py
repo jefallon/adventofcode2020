32a8bac1c9eb590abd8b21d8d1b8ad95afbe2c29
= <=>
2020-12-13 19:39:30 -0500

    Day 13b wrong

--- Day13b.py
+++ Day13b.py
@@ -10,13 +10,22 @@ def find_consecutive(buses):
     first_bus = buses[0][1]
     first_time = buses[0][0]
     while True:
+        # print(first_time)
         for minutes, bus in buses[1:]:
             if (first_time + minutes) % bus:
+                first_bus *= bus
+                buses.remove((minutes, bus))
+                # print(buses)
+                # print(first_bus)
+                # print(first_time)
+
+                # print(bus)
                 break
         else:
             return first_time
         first_time += first_bus
 
 if __name__ == '__main__':
-    buses = get_buses('Advent13.txt')
+    # buses = get_buses('Advent13.txt')
+    buses = [(0,17), (2,13), (3,19)]
     print(find_consecutive(buses))
