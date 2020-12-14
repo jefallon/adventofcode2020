fa6a9b4e8cd517492a8eb821817b3e93357580ff
Joshua Fallon <fallonje@adelphia.net>
2020-12-13 20:46:44 -0500

    Day 13

--- Day13b.py
+++ Day13b.py
@@ -10,22 +10,21 @@ def find_consecutive(buses):
     first_bus = buses[0][1]
     first_time = buses[0][0]
     while True:
-        # print(first_time)
         for minutes, bus in buses[1:]:
             if (first_time + minutes) % bus:
+                break
+            else:
                 first_bus *= bus
                 buses.remove((minutes, bus))
-                # print(buses)
-                # print(first_bus)
-                # print(first_time)
-
-                # print(bus)
                 break
         else:
             return first_time
+        if len(buses) == 1:
+            return(first_time)
         first_time += first_bus
 
+
+
 if __name__ == '__main__':
-    # buses = get_buses('Advent13.txt')
-    buses = [(0,17), (2,13), (3,19)]
+    buses = get_buses('Advent13.txt')
     print(find_consecutive(buses))
