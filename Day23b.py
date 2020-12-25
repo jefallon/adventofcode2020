3c26f414431bd4dec8cd7f3756b1a8c601b2ab5d
Joshua Fallon <fallonje@adelphia.net>
2020-12-25 15:19:38 -0500

    Day 23 fix part b

--- Day23b.py
+++ Day23b.py
@@ -1,35 +1,42 @@
-from collections import deque
+def cups_game(cycle, rounds):
+    cur_val = cycle[0]
+    max_val = 10**6
 
-class cupsGame:
-    def __init__(self, cups_list, max_cup):
-        self.cups_list = deque(cups_list+list(range(10,max_cup+1)))
-        self.max = max_cup
+    cycle_dict = dict(zip(cycle, cycle[1:]))
+    cycle_dict[cycle[-1]] = cur_val
 
-    def make_move(self):
-        this_cup = self.cups_list.popleft()
-        dest = this_cup - 1
-        picked_up = [self.cups_list.popleft() for _ in range(3)]
-        if dest == 0:
-            dest = self.max
-        while dest in picked_up:
-            dest -= 1
-            if dest == 0:
-                dest = self.max
-        #cProfile says I'm spending all my time here, which makes sense
-        drop = self.cups_list.index(dest)+1
-        for _ in range(3):
-            self.cups_list.insert(drop, picked_up.pop())
-        self.cups_list.append(this_cup)
+    def pick_up():
+        pick = [cycle_dict[cur_val]]
+        for _ in range(2):
+            pick.append(cycle_dict[pick[-1]])
+        return pick
+
+    while rounds:
+        rounds -= 1
+        pick = pick_up()
+
+        next_val = cur_val-1
+        while next_val <= 0 or next_val in pick:
+            next_val -= 1
+            if next_val <= 0:
+                next_val = max_val
+
+        cycle_dict[cur_val] = cycle_dict[pick[-1]]
+        cycle_dict[pick[-1]] = cycle_dict[next_val]
+        cycle_dict[next_val] = pick[0]
 
 
-    def play_game(self, rounds):
-        pos = 0
-        while rounds:
-            rounds -= 1
-            self.make_move()
-        idx = self.cups_list.index(1)
-        return [self.cups_list[idx+1],self.cups_list[idx+2]]
+        cur_val = cycle_dict[cur_val]
+
+    return cycle_dict
+
+def seed_game(cups_list, max):
+    long_cycle = cups_list + list(range(10, max+1))
+    return long_cycle
 
 if __name__ == '__main__':
-    game = cupsGame([7,1,6,8,9,2,5,4,3], 10**6)
-    # print(play_game(cups_list, 100))
+    game_state = seed_game([7,1,6,8,9,2,5,4,3], 10**6)
+    end_state = cups_game(game_state, 10**7)
+    star_1 = end_state[1]
+    star_2 = end_state[star_1]
+    print(star_1*star_2)
